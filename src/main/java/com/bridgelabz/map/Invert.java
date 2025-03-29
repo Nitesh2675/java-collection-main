50a5d692bd4412c8f38cabe5db4fc537bf72b7ec
niteshxx <nitesh1136.be21@chitkarauniversity.edu.in>
2025-03-29 09:38:15 +0530

    [Nitesh] ADD: Inverted map

--- src/main/java/com/bridgelabz/map/Invert.java
+++ src/main/java/com/bridgelabz/map/Invert.java
@@ -1,10 +1,29 @@
 package com.bridgelabz.map;
 
-import java.util.HashMap;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.Map;
+import java.util.*;
 
 public class Invert {
     public static void main(String[] args){
         HashMap<String,Integer> map=new HashMap<>();
+        map.put("A",1);
+        map.put("B",2);
+        map.put("C",1);
 
+
+        HashMap<Integer,List<String>> invertedMap=new HashMap<>();
+        for(Map.Entry<String,Integer> entry:map.entrySet()){
+            Integer value=entry.getValue();
+            String key =entry.getKey();
+
+          if(!invertedMap.containsKey(value)){
+              invertedMap.put(value,new ArrayList<>());
+          }
+          invertedMap.get(value).add(key);
+
+        }
+        System.out.println(invertedMap);
     }
 }
